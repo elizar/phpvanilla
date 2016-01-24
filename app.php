fb0a0258f2e7307601b3fcf4b0089301ec095aca
Elizar Pepino <jupenz@gmail.com>
2016-01-25 02:09:46 +0800

    Some changes
    Signed-off-by: Elizar Pepino <jupenz@gmail.com>

--- app.php
+++ app.php
@@ -3,59 +3,11 @@
 class App
 {
     private $routes = array();
+    public $notFoundHandler;
 
     function __construct()
     {
-        // Todo: write some awesome shit in here
-    }
-
-    function run($method, $path)
-    {
-        $path_segments  = $this->createSegmentsFromPath($path);
-        $segments_count = count($path_segments);
-        $request_method = strtolower($method);
-
-        // set initial params and handler
-        $_params = [];
-        $matched_handler = [$this, 'notFoundHandler'];
-
-        if (isset($this->routes[$request_method][$segments_count])) {
-            $paths = $this->routes[$request_method][$segments_count];
-
-            foreach ($paths as $route => $handler) {
-                $route_segments = $this->createSegmentsFromPath($route);
-                $segments = [];
-                $params   = [];
-
-                foreach ($route_segments as $segment) {
-                    if (substr($segment, 0, 1) == ':') {
-                        $index = array_search($segment, $route_segments);
-                        $params[str_replace(':', '', $segment)] = $path_segments[$index];
-                    } else {
-                        $segments[] = $segment;
-                    }
-                }
-
-                $matched_segments = array_filter(
-                    $segments,
-                    function ($segment) use ($path_segments, $route_segments) {
-                        $index = array_search($segment, $route_segments);
-                        // include only if it matches
-                        return $segment == $path_segments[$index];
-                    }
-                );
-
-                if (count($matched_segments) == count($segments)) {
-                    $matched_handler = $handler;
-                }
-
-                if (count($params) > 0) {
-                    $_params = $params;
-                }
-            }
-        }
-
-        return $matched_handler($_params);
+        $this->notFoundHandler = [$this, 'defaultNotFoundHandler'];
     }
 
     function get($path, $handler)
@@ -63,18 +15,63 @@ class App
         return $this->addRoute('get', $path, $handler);
     }
 
-    function getRoutes()
+    function post($path, $handler)
     {
-        return $this->routes;
+        return $this->addRoute('post', $path, $handler);
     }
 
-    private function notFoundHandler()
+    function run($method, $path)
+    {
+        $request_segments  = $this->createSegmentsFromPath($path);
+        $segments_count = count($request_segments);
+        $request_method = strtolower($method);
+
+        // Set initial request params
+        $params = [];
+        // Set initial notFound handler
+        $matched_handler = $this->notFoundHandler;
+
+        if (isset($this->routes[$request_method][$segments_count])) {
+            $paths = $this->routes[$request_method][$segments_count];
+
+            foreach ($paths as $route => $handler) {
+                $route_segments = $this->createSegmentsFromPath($route);
+
+                $segments = $this->getSegmentsFromRouteSegments($route_segments);
+                // Check if matched, then set $matched_handler
+                if ($this->segmentsMatched($segments, $route_segments, $request_segments)) {
+                    $matched_handler = $handler;
+                }
+
+                // Set request params
+                $params = $this->getParamsFromSegments($route_segments, $request_segments);
+            }
+        }
+
+        return $matched_handler($params);
+    }
+
+    /**
+     * -------------------------------------------------------------
+     * PRIVATE METHODS
+     * -------------------------------------------------------------
+     *
+     */
+    private function defaultNotFoundHandler()
     {
         echo 'Oh Snap! - We couldn\'t find what you\'re looking for';
         echo '<br>';
         echo 'click <a href="/" title="Home">here</a> to go back.';
     }
 
+    /*
+     * Adds route handler
+     *
+     * @param string $method
+     * @param string $path
+     * @param function $handler
+     * @return self for chainability
+     */
     private function addRoute($method, $path, $handler)
     {
         $segments = count($this->createSegmentsFromPath($path));
@@ -83,7 +80,7 @@ class App
         if (isset($this->routes[$method][$segments][$path]) &&
             is_callable($this->routes[$method][$segments][$path])
         ) {
-            return;
+            return $this;
         }
 
         $this->routes[$method][$segments][$path] = $handler;
@@ -91,8 +88,96 @@ class App
         return $this;
     }
 
+    /*
+     * Transform a path into an array of segments
+     *
+     * @param string $path
+     * @return array
+     */
     private function createSegmentsFromPath($path)
     {
         return explode('/',preg_replace('/(^\/|\/$)/','', $path));
+    }
+
+    /*
+     * Gets non parameter segments from route's segments
+     *
+     * Given the following route `/users/:id/friends`,
+     * creates ['users', ':id', 'friends'] segments array.
+     *
+     * This method will filter those non-parameter segment and
+     * return only those segments i.e ['users', 'friends']
+     *
+     * @param array $route_segments An array of segments from route
+     * @return array A list of non-parameter segments
+     */
+    private function getSegmentsFromRouteSegments($route_segments)
+    {
+        $segments = [];
+
+        foreach ($route_segments as $segment) {
+            if (substr($segment, 0, 1) != ':') {
+                $segments[] = $segment;
+            }
+        }
+
+        return $segments;
+    }
+
+    /*
+     * Gets all the param segments from route and
+     * match it up with param values from request
+     * path.
+     *
+     * Consider the folowing request path:
+     * `/user/31337/friend/12345`
+     * and the matching route's path:
+     * `/user/:id/friend/:fid`
+     *
+     * Results: ['id' => 31337, 'fid' => 12345]
+     *
+     * @param array $route_segments
+     * @param array $request_segments
+     * @return array
+     */
+    private function getParamsFromSegments($route_segments, $request_segments)
+    {
+        $params = [];
+
+        foreach ($route_segments as $segment) {
+            if (substr($segment, 0, 1) == ':') {
+                $index = array_search($segment, $route_segments);
+                $params[str_replace(':', '', $segment)] = $request_segments[$index];
+            }
+        }
+
+        return $params;
+    }
+
+    /*
+     * Validates if a selected array of segments
+     * matches the current request's segments
+     *
+     * @param array $segments
+     * @param array $route_segments
+     * @param array $request_segments
+     * @return bool
+     */
+    private function segmentsMatched($segments, $route_segments, $request_segments)
+    {
+        $matched_segments = array_filter(
+            $segments,
+            function ($segment) use ($request_segments, $route_segments) {
+                $index = array_search($segment, $route_segments);
+                // include only if it matches
+                return $segment == $request_segments[$index];
+            }
+        );
+
+        if (count($matched_segments) == count($segments)) {
+            return true;
+        }
+
+        return false;
     }
 }
--- index.php
+++ index.php
@@ -4,15 +4,23 @@ require_once './app.php';
 $path_info      = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/';             // defaults to '/'
 $request_method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'get'; // defaults to 'get'
 
+// Init app like a bitch!
 $app = new App();
 
+// Overrides default not found handler
+$app->notFoundHandler = function () {
+    echo 'Not Found Bitches!';
+};
+
+// Adds routes
 $app->get('/hello/:name', function ($params) {
-        echo 'Hello, ';
+        echo 'Hello, ' . ucfirst($params['name']);
     })
     ->get('/hello/world/asd', function () {
         echo 'hello world asd';
     })
     ->get('/', function () {
-        echo 'Home';
+        header('content-type: application/json');
+        echo json_encode($_SERVER);
     })
     ->run($request_method, $path_info);
